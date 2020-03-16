18d6524b55ddd3f3e914b8df4052b4f28b7d7a7f
jetreal <41300107+jetreal@users.noreply.github.com>
2020-03-16 16:12:39 +0300

    del uploads images from unother project

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,9 +4,6 @@ const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 
 const User = require('../models/user');
-const TransactionHistory = require('../models/transaction');
-
-
 
 
 exports.get_all_users = (req, res, next) => {
