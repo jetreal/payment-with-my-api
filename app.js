99e25843c590c296abd26e268204fb5a6201369e
jetreal <41300107+jetreal@users.noreply.github.com>
2020-03-27 21:37:29 +0300

    change port

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ app.get('/', function(req, res) {
   res.render('index')
 })
 
+if (process.env.NODE_ENV === 'production') {
+  
+}
+
 app.use((req, res, next) => {
   const error = new Error('Not found');
   error.status = 404;
